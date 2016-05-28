314e4519af8fdb1a4fbbfaf63b089c6f3a8141e2
Sebastien N <snicolet@student.42.fr>
2016-05-28 03:52:37 +0200

    ajout de draw_matrix_multiply_axes

--- draw_matrix_multiply_matrix.c
+++ draw_matrix_multiply_matrix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_matrix_multiply_matrix.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 16:33:40 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/08 18:20:56 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/28 03:41:20 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,23 @@ t_matrix	draw_matrix_multiply_matrix(t_matrix m1, const t_matrix *m2)
 	m1.offset.y += m2->offset.y;
 	m1.offset.z += m2->offset.z;
 	return (m1);
+}
+
+t_matrix	draw_matrix_multiply_axes(t_vector axes, t_vector scale,
+	t_vector offset)
+{
+	t_matrix	final;
+	t_matrix	mx;
+	t_matrix	my;
+	t_matrix	mz;
+	t_vector	o;
+
+	o = (t_vector){0.0, 0.0, 0.0};
+	mx = draw_make_matrix_x(o, axes.x, scale);
+	mx = draw_make_matrix_y(o, axes.y, scale);
+	mx = draw_make_matrix_z(o, axes.z, scale);
+	final = draw_matrix_multiply_matrix(mx, &my);
+	final = draw_matrix_multiply_matrix(final, &mz);
+	final.offset = offset;
+	return (final);
 }
--- headers/draw.h
+++ headers/draw.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/29 13:27:50 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/21 19:43:28 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/28 03:41:37 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,8 @@ t_matrix		draw_make_matrix_iso(int x, int y, int w, int h);
 t_matrix		draw_make_matrix_ortho(t_vector s, t_vector c);
 t_matrix		draw_matrix_multiply_matrix(t_matrix m1, const t_matrix *m2);
 t_matrix		draw_make_matrix_identity(void);
+t_matrix		draw_matrix_multiply_axes(t_vector axes, t_vector scale,
+	t_vector offset);
 
 t_vector		draw_make_vector(float x, float y, float z);
 t_vector		draw_vector_transform(t_vector v, const t_matrix *m);
