b07e7f21ce07590b78059aea4016a7e1b28487af
Sebastien N <snicolet@student.42.fr>
2016-06-10 19:04:59 +0200

    fix

--- draw_vector_dist.c
+++ draw_vector_dist.c
@@ -6,13 +6,13 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/10 18:59:04 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/10 19:02:51 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/10 19:04:42 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw.h"
 
-double		draw_v4d_dist(t_v4d a, t_v4d b)
+double		draw_v4d_dist(const t_v4d a, const t_v4d b)
 {
 	const double	dx = (b.x - a.x) * (b.x - a.x);
 	const double	dy = (b.y - a.y) * (b.y - a.y);
