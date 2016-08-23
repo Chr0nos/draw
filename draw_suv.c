e7633dc9580867801316b94c2caab05da2da16ae
Sebastien NICOLET <snicolet@student.42.fr>
2016-08-23 22:29:03 +0200

    up

--- draw_suv.c
+++ draw_suv.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/03 17:20:24 by snicolet          #+#    #+#             */
-/*   Updated: 2016/08/23 21:41:49 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/23 22:28:05 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,10 @@ unsigned int	draw_suv(SDL_Surface *surface, t_v2f uv)
 ** uv MUST be bettween 0 and 1, this function will NOT clamp values
 ** step = how many UV between 2 pixels
 ** color_a = the current pixel in the uv texture (not smooth at all)
-** color_b = the color of the near pixel to interpolate
+** color = the color of the near pixel to interpolate
+**    it has a x and y for each near sides
 ** lerp_pc = a number range(0.0 - 0.5) to know the value of the degrade to to
+**    lerp.z is for the global interpolation bewtween lerp.x and lerp.y
 **   with between color_a and color_b (0.0 = color_a)
 ** frac = fractional part of the position:
 ** - the idea is to considerate the center of the current pixel as 0, 0 uv
@@ -32,7 +34,10 @@ unsigned int	draw_suv(SDL_Surface *surface, t_v2f uv)
 **   will be used for the color_lerp
 ** ----
 ** function logic:
-** ... who knows.... i dont even know what i'm doing !!!
+**    r = fractional_part(uv / texture_size) - 0.5f
+**    if (r < 0.0f) then use the pixel left or down of the current
+**    else use the pixel right or up the current
+**    do a lerp bewtween the 2 selected pixels (color.x and color.y)
 */
 
 #include <stdio.h>
@@ -51,18 +56,21 @@ unsigned int	draw_suv_smooth(SDL_Surface *surface, t_v2f uv)
 	t_v3f				lerp_pc;
 
 	pxuv = (t_v2f){
-		geo_fract(uv.x * surface->w) - 0.5f,
-		geo_fract(uv.y * surface->h) - 0.5f
+		(geo_fract(uv.x * surface->w) - 0.5f) * 2.0f,
+		(geo_fract(uv.y * surface->h) - 0.5f) * 2.0f
 	};
-	color.x = draw_suv(surface, (t_v2f){clamp(uv.x + ((pxuv.x > 0.0f) ? step.x : -step.x)), uv.y});
-	color.y = draw_suv(surface, (t_v2f){uv.x, clamp(uv.y + ((pxuv.x > 0.0f) ? step.y : -step.y))});
+	color.x = draw_suv(surface,
+		(t_v2f){clamp(uv.x + ((pxuv.x > 0.0f) ? step.x : -step.x)), uv.y});
+	color.y = draw_suv(surface,
+		(t_v2f){uv.x, clamp(uv.y + ((pxuv.x > 0.0f) ? step.y : -step.y))});
 	color.x = 0xff0000;
 	color.y = 0x0000ff;
 //	printf("%f %f lerp: %f\n", (double)pxuv.x, (double)pxuv.y, (double)lerp_pc.x);
 	lerp_pc.x = ABS(pxuv.x);
 	lerp_pc.y = ABS(pxuv.y);
 	lerp_pc.z = (lerp_pc.x + lerp_pc.y) * 0.5f;
-	color.z = draw_color_lerp(draw_color_lerp(color_a, color.x, lerp_pc.x),
-		color.y, lerp_pc.z);
+	color.x = draw_color_lerp(color_a, color.x, geo_clamp(lerp_pc.x, 0.0f, 0.5f));
+	color.y = draw_color_lerp(color_a, color.y, geo_clamp(lerp_pc.y, 0.0f, 0.5f));
+	color.z = draw_color_lerp(color.x, color.y, geo_clamp(lerp_pc.z, 0.0f, 0.5f));
 	return (color.z | (color_a & 0xff000000));
 }
