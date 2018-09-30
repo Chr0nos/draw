f8546970830d98124a8503ea26c6c25a2d30111c
Sebastien NICOLET <snicolet@student.42.fr>
2018-09-30 18:01:34 +0200

    added a cross for t_v3f

--- geo/geo_cross.c
+++ geo/geo_cross.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/03 13:51:58 by alhote            #+#    #+#             */
-/*   Updated: 2016/08/03 13:57:38 by alhote           ###   ########.fr       */
+/*   Updated: 2018/09/30 18:00:41 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,12 @@ t_v4d		geo_crossv4(t_v4d a, t_v4d b)
 		a.x * b.y - a.y * b.x,
 		a.w * b.w
 	});
+}
+
+t_v3f		geo_cross(t_v3f a, t_v3f b)
+{
+	return ((t_v3f){
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x});
 }
--- headers/geo.h
+++ headers/geo.h
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/23 19:19:23 by qloubier          #+#    #+#             */
-/*   Updated: 2017/06/05 16:58:55 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/09/30 18:01:14 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ t_v3f			geo_invv(t_v3f a);
 float			geo_dotv(t_v3f a, t_v3f b);
 t_v3f			geo_multv(t_v3f a, t_v3f b);
 t_v3f			geo_normv(const t_v3f v);
+t_v3f			geo_cross(t_v3f a, t_v3f b);
 
 float			geo_dotv4f(t_v4f a, t_v4f b);
 double			geo_dotv4(t_v4d a, t_v4d b);
